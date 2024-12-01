e4a7e80b058c8930ef47222c3fe311f073c96a5a
ryuji55 <rt.october.2@icloud.com>
2024-12-01 10:55:31 +0900

    helmet導入・node_modulesのignore

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,11 +1,13 @@
 import express, { Request, Response } from "express";
 import cors from "cors";
+import helmet from "helmet";
 
 const app = express();
 const port = process.env.PORT || 3001;
 
 app.use(cors());
 app.use(express.json());
+app.use(helmet());
 
 app.get("/api/test", (req: Request, res: Response) => {
   res.json({ message: "Hello from Express + TypeScript!" });
