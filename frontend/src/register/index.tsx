ece9a2dccbe748f306a7131cd79e1a64daab5dcc
ryuji55 <rt.october.2@icloud.com>
2024-12-01 10:34:59 +0900

    ログインページ・登録ページに入力フォームを実装

--- frontend/src/register/index.tsx
+++ frontend/src/register/index.tsx
@@ -1,13 +1,32 @@
 import { FC } from "react";
+import { SubmitHandler, useForm } from "react-hook-form";
 import { useNavigate } from "react-router";
+
+type IFormInput = {
+  firstName: String;
+  email: String;
+  password: String;
+};
 
 export const RegisterPage: FC = () => {
   const navigate = useNavigate();
   const goToHomePage = () => navigate("/");
   const goToLoginPage = () => navigate("/login");
+
+  const { register, handleSubmit } = useForm<IFormInput>();
+  const onSubmit: SubmitHandler<IFormInput> = (data) => console.log(data);
   return (
     <>
       <h1>登録ページ</h1>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <label>名前</label>
+        <input {...register("firstName")} />
+        <label>メールアドレス</label>
+        <input {...register("email")} />
+        <label>パスワード</label>
+        <input {...register("password")} />
+        <input type="submit" />
+      </form>
       <button onClick={goToHomePage}>ホームページ</button>
       <button onClick={goToLoginPage}>ログインページ</button>
     </>
