c2b0dab46b485b9bb6c6968a4215b06a907e8699
ryuji55 <rt.october.2@icloud.com>
2024-12-01 09:49:12 +0900

    仮置きのログイン有無双方のページ・react-routerの初期設定

---
+++ frontend/src/login/index.tsx
@@ -0,0 +1,9 @@
+import { FC } from "react";
+
+export const LoginPage: FC = () => {
+  return (
+    <>
+      <h1>ログインページ</h1>
+    </>
+  );
+};
---
+++ frontend/src/user/noAuth/index.tsx
@@ -0,0 +1,9 @@
+import { FC } from "react";
+
+export const NoAuthPage: FC = () => {
+  return (
+    <>
+      <h1>ログインしていないページ</h1>
+    </>
+  );
+};
