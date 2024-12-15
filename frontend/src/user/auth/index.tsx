fe31bb59ecd9f392d0f4c36edc86136a5b679002
ryuji55 <rt.october.2@icloud.com>
2024-12-15 20:15:52 +0900

    ログアウト処理のエラーハンドリング

--- frontend/src/user/auth/index.tsx
+++ frontend/src/user/auth/index.tsx
@@ -1,25 +1,34 @@
 import axios from "axios";
-import { FC } from "react";
+import { FC, useState } from "react";
 import { useNavigate } from "react-router";
 
 export const AuthPage: FC = () => {
   const navigate = useNavigate();
   const goToLoginPage = () => navigate("/login");
+  const [error, setError] = useState<string | null>(null);
+
   const handleLogout = async () => {
     try {
-      const response = await axios.post("/api/auth/logout");
-      if (response.status === 200) {
-        console.log("ログアウト成功");
-        goToLoginPage();
+      await axios.post("/api/auth/logout");
+      goToLoginPage();
+    } catch (error: any) {
+      if (error.response) {
+        // サーバーからのエラーレスポンス
+        const message = error.response.data?.error || "ログインに失敗しました";
+        setError(message);
+      } else if (error.request) {
+        // リクエストは送信されたがレスポンスを受け取れなかった
+        setError("サーバーからの応答がありません");
       } else {
-        console.error("ログアウト失敗");
+        // リクエストの作成時に何か問題が発生
+        setError("予期せぬエラーが発生しました");
       }
-    } catch (error) {
-      console.error("ログアウトエラー:", error);
     }
   };
+
   return (
     <>
+      <p>{error}</p>
       <h1>ログインしているページ</h1>
       <button onClick={handleLogout}>ログアウト</button>
     </>
