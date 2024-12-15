108ad02791657b785d1799a47f51cea97a85f3f4
ryuji55 <rt.october.2@icloud.com>
2024-12-15 12:22:10 +0900

    ログアウト処理。session,cookieの削除

--- backend/src/routes/api/auth.ts
+++ backend/src/routes/api/auth.ts
@@ -13,4 +13,24 @@ router.get("/me", (req: any, res: any) => {
   res.json({ user: req.user });
 });
 
+router.post("/logout", (req: any, res: any) => {
+  // passportのログアウト処理
+  req.logout((err: any) => {
+    if (err) {
+      return res.status(500).json({ error: "ログアウトに失敗しました" });
+    }
+    // セッション全体を破棄
+    req.session.destroy((err: any) => {
+      if (err) {
+        return res
+          .status(500)
+          .json({ error: "セッションの破棄に失敗しました" });
+      }
+      // Cookieも削除
+      res.clearCookie("connect.sid");
+      res.json({ message: "ログアウトしました" });
+    });
+  });
+});
+
 export { router as authRouter };
--- frontend/src/user/auth/index.tsx
+++ frontend/src/user/auth/index.tsx
@@ -1,9 +1,27 @@
+import axios from "axios";
 import { FC } from "react";
+import { useNavigate } from "react-router";
 
 export const AuthPage: FC = () => {
+  const navigate = useNavigate();
+  const goToLoginPage = () => navigate("/login");
+  const handleLogout = async () => {
+    try {
+      const response = await axios.post("/api/auth/logout");
+      if (response.status === 200) {
+        console.log("ログアウト成功");
+        goToLoginPage();
+      } else {
+        console.error("ログアウト失敗");
+      }
+    } catch (error) {
+      console.error("ログアウトエラー:", error);
+    }
+  };
   return (
     <>
       <h1>ログインしているページ</h1>
+      <button onClick={handleLogout}>ログアウト</button>
     </>
   );
 };
