da5fcfeb5b121168eebe783aa9d4efe39fa74c19
ryuji55 <rt.october.2@icloud.com>
2025-01-03 15:49:26 +0900

    seedのパスワードをハッシュ化した

--- backend/prisma/seed/index.ts
+++ backend/prisma/seed/index.ts
@@ -1,19 +1,21 @@
+import bcrypt from "bcrypt";
 // backend/prisma/seed.ts
 import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 async function main() {
+  const hashedPassword = await bcrypt.hash("password", 10);
   await prisma.users.createMany({
     data: [
       {
         email: "seedData1@example.com",
         firstName: "seedData1",
-        password: "password",
+        password: hashedPassword,
       },
       {
         email: "seedDate2@example.com",
         firstName: "seedData2",
-        password: "password",
+        password: hashedPassword,
       },
     ],
   });
